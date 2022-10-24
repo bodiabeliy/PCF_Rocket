e368a5dd69e4fc447822d90c2572a613e899099d
bodiabeliy <bboda2106@gmail.com>
2022-10-24 20:10:05 +0300

    finish dynamic data getting (L2 Modules)

--- Rocket_models/components/InfoTree.tsx
+++ Rocket_models/components/InfoTree.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react'
-import { ForkOutlined, NodeCollapseOutlined, NodeExpandOutlined } from '@ant-design/icons';
-import { Card, List, Popover } from 'antd';
+import { NodeExpandOutlined } from '@ant-design/icons';
+import { Card } from 'antd';
 import Meta from 'antd/lib/card/Meta';
 import { Tree, TreeNode } from 'react-organizational-chart';
 
-import InfoTreePreviewInfo from './InfoTreePreviewInfo'
 import L3Modules from './L3Modules'
 
 import './index.css'
@@ -20,36 +19,16 @@ const InfoTree = (infoTreeData: any) => {
   const [selectedParentCategory, setSelectedParentCategory] = React.useState("")
   const [isSelectedParentCategory, setIsSelectedParentCategory] = React.useState<boolean>(false)
 
-
-  const groupedCtegories = (groupedElements: any) => {
-    groupedElements.map((element: any) => {
-
-      const groupedSubCategories = groupBy(element.L3Items, "Category")
-      groupedElements  =[groupedSubCategories]
-      setL2ParentNode(groupedElements);
-      return groupedSubCategories;
-    })
-  }
-
-  const openParentTrees = (parentCategoryName: any) => {
-    console.log("parentNode", parentNode);
-
+  const openParentTrees = (parentCategoryObject: any) => {
+    
     for (let parentCategory of parentNode) {      
-        if (parentCategory.Name == parentCategoryName) {
-          console.log("parentCategoryName", parentCategoryName);
+        if (parentCategory.Name == parentCategoryObject.Name) {
+          setL2ParentNode(groupBy(parentCategoryObject.L3Items, "Category"))
           setSelectedParentCategory(parentCategory.Name)
           setIsSelectedParentCategory(!isSelectedParentCategory)
-          groupedCtegories(parentNode)
         }
     }
   }
-
-  
-
-
-
-
-
 
   const groupBy = (array: any, key: any) => {
     return array.reduce((result: any, currentValue: any) => {
@@ -92,17 +71,15 @@ const InfoTree = (infoTreeData: any) => {
               >
                 <Meta
                   avatar={
-                    <NodeExpandOutlined onClick={() => openParentTrees(L2Item.Name)} />}
+                    <NodeExpandOutlined onClick={() => openParentTrees(L2Item)} />}
                   title={L2Item.Name}
                   description={L2Item.Description}
                 />
-                {L2ParentNode[0] && L2Item.Name == selectedParentCategory && isSelectedParentCategory ?
+                {L2ParentNode && L2Item.Name == selectedParentCategory && isSelectedParentCategory ?
                   <>
-                    <TreeNode className='infoTreeCardSubChildren' label={Object.entries(L2ParentNode[0]).map((Categories: any, indx: any) => {
-
+                    <TreeNode className='infoTreeCardSubChildren' label={Object.entries(L2ParentNode).map((Categories: any, indx: any) => {                      
                       return (
-                        
-                        <L3Modules categoryModules={Categories} categoryIndex={indx} categoriesList={L2ParentNode[0]} />
+                        <L3Modules categoryModules={Categories} categoryIndex={indx} categoriesList={L2ParentNode} />
                       )
                     })}>
                     </TreeNode>
